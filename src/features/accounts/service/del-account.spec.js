13df065121023069f7bedc8700a8498a4459d5b0
joão melo <j@melo.plus>
2021-05-16 17:34:57 -0300

    fix: test logic synced with updates on fixtures

--- src/features/accounts/service/del-account.spec.js
+++ src/features/accounts/service/del-account.spec.js
@@ -16,7 +16,7 @@ describe('del account use case', () => {
 
   test('call del actions on both accounts and balances services', async () => {
     const accountId = '8a502ea4-2532-409a-8082-d4e88aaadd03';
-    const balancesIds = ['82362731-b916-4e4f-b810-fb6bdee8b825', '137ce1a3-592e-4979-b47c-27ad796e5dce'];
+    const balancesIds = balances.filter(b => b.accountId === accountId).map(b => b.id);
 
     await delAccountCase(accountsService, balancesService, accountId);
 
