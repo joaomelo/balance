d2f8c5cd6ec702c486fc3d5315c46e966f1f768f
joão melo <j@melo.plus>
2021-06-12 10:29:52 -0300

    chore: removing vanilla design styles

--- src/app/styles/global.js
+++
@@ -1,8 +0,0 @@
-import { createGlobalStyle } from 'styled-components';
-import { ResetStyles } from './reset';
-import { TokenStyles } from './token';
-
-export const GlobalStyles = createGlobalStyle`
-  ${ResetStyles}
-  ${TokenStyles}
-`;
