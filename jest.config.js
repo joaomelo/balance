35f63cdded2db0439289bebb1f71fc1b5d5aaf5f
joão melo <j@melo.plus>
2021-06-21 22:06:56 -0300

    chore: increased timeout for playwirght

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.js', 'src/**/*.jsx'],
   coverageDirectory: 'tests/coverage',
+  testTimeout: 30000, // e2e tests more time than the default 5s
   testMatch: ['**/__tests__/**/*.js?(x)', '**/src/**/?(*.)+(spec|test).js?(x)']
 };
