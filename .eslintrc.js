d13da7c975a7016649f938c257a12d42641d495b
joão melo <j@melo.plus>
2021-11-27 10:10:21 -0300

    chore: adopting prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,37 @@
 module.exports = {
   root: true,
-  parser: '@babel/eslint-parser',
-  plugins: [
-    'jest',
-    'jest-formatting'
-  ],
+  parser: "@babel/eslint-parser",
+  plugins: ["jest", "jest-formatting"],
   extends: [
-    'standard',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:jest/recommended',
-    'plugin:jest/style',
-    'plugin:jest-formatting/recommended'
+    "standard",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jest/recommended",
+    "plugin:jest/style",
+    "plugin:jest-formatting/recommended",
+    "prettier",
   ],
   rules: {
-    semi: ['error', 'always'],
-    'no-console': [
-      'warn', {
-        allow: ['warn', 'error', 'info']
-      }
+    semi: ["error", "always"],
+    "no-console": [
+      "warn",
+      {
+        allow: ["warn", "error", "info"],
+      },
     ],
-    'no-debugger': 'warn',
-    'import/export': 0, // erroneous report "no named exports" when exporting from jsx
-    'react/jsx-uses-react': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 0
+    "no-debugger": "warn",
+    "import/export": 0, // erroneous report "no named exports" when exporting from jsx
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": 0,
   },
   settings: {
     react: {
-      version: 'detect'
-    }
+      version: "detect",
+    },
   },
   env: {
     browser: true,
-    'jest/globals': true
-  }
+    "jest/globals": true,
+  },
 };
